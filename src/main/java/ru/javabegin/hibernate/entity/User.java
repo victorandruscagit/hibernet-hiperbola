36661e77471412d44bcad4202fafa6d401679522
victorandruscagit <victor.v.andrusca@gmail.com>
2021-09-29 10:56:00 +0300

    entity class and lombok added

---
+++ src/main/java/ru/javabegin/hibernate/entity/User.java
@@ -0,0 +1,29 @@
+package ru.javabegin.hibernate.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user_data", schema = "todolist", catalog = "postgres")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String username;
+
+    @Column(name = "userpassword")
+    private String password;
+
+
+}
