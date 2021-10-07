54dd276c32988e18edc58b28a8b961bb75075cc0
victorandruscagit <victor.v.andrusca@gmail.com>
2021-10-07 16:53:54 +0300

    played with CrteriaAPI

--- src/main/java/ru/javabegin/hibernate/Main.java
+++ src/main/java/ru/javabegin/hibernate/Main.java
@@ -1,26 +1,32 @@
 package ru.javabegin.hibernate;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import lombok.extern.log4j.Log4j2;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 import ru.javabegin.hibernate.entity.User;
 
+import java.util.List;
+
+@Log4j2
 public class Main {
 
     public static void main(String[] args) {
+        log.info("Hibernate tutorial started");
         Session session = HibernateUtil.getSessionFactory().openSession();
-        session.getTransaction().begin();
-        User user = new User();
-        user.setEmail("zcalin1@ff.ascom");
-        user.setUsername("zcalingosia");
-        user.setPassword("vera2");
-        session.save(user);
-        session.getTransaction().commit();
-        System.out.println("user.getId() = " + user.getId());
+
+
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
+        Root<User> root = criteriaQuery.from(User.class);
+        criteriaQuery.select(root);
+        Query<User> query = session.createQuery(criteriaQuery);
+        List<User> resultList = query.getResultList();
+        log.info(resultList);
         session.close();
         HibernateUtil.close();
-
-
-
-
 
 
     }
