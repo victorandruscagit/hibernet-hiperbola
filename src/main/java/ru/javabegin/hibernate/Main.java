08547c3344d9856a59467c3a5911abfb0231b5f4
victorandruscagit <victor.v.andrusca@gmail.com>
2021-10-11 15:38:44 +0300

    get, find, load on session

--- src/main/java/ru/javabegin/hibernate/Main.java
+++ src/main/java/ru/javabegin/hibernate/Main.java
@@ -2,7 +2,7 @@ package ru.javabegin.hibernate;
 
 import lombok.extern.log4j.Log4j2;
 import org.hibernate.Session;
-import org.hibernate.query.NativeQuery;
+import ru.javabegin.hibernate.entity.User;
 
 @Log4j2
 public class Main {
@@ -10,24 +10,10 @@ public class Main {
     public static void main(String[] args) {
         log.info("Hibranta SQL started");
         Session session = HibernateUtil.getSessionFactory().openSession();
-        NativeQuery<Object[]> nativeQuery = session.createNativeQuery("select\n" +
-                "count(u.id),\n" +
-                "substring(u.email,position('@' in u.email) +1,length(u.email)) as domainEmail\n" +
-                "\n" +
-                "from todolist.user_data u\n" +
-                "where u.email like '%@%'\n" +
-                "group by domainEmail;");
-
-        for (Object[] o : nativeQuery.getResultList()) {
-
-            log.info( o[0]);
-            log.info( o[1]);
-            log.info("-----");
-
-
-        }
-        //log.info(resultList);
-
+        //User user = session.get(User.class, 10025L);
+        User user = session.get(User.class, 20040000L);
+       /* log.info("is " + user.getUsername());
+*/
         session.close();
         HibernateUtil.close();
 
